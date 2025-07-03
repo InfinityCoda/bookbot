cc79ea7cc27ce1ff7899e33ec6dce481e541acaf
InfinityCoda <charles.cullymore@gmail.com>
2025-07-02 22:21:14 -0400

    Remove hardcoded path to book. Now takes the path to a book file as an arg.

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from stats import *
+import sys
 
-def get_book_text():
-    with open("./books/frankenstein.txt") as frankenstein:
-        file_contents = frankenstein.read()
+def get_book_text(book_file):
+    with open(book_file) as book:
+        file_contents = book.read()
         return file_contents
     
 
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    book_text = get_book_text()
+    print(f"Analyzing book found at {sys.argv[1]}...")
+    book_text = get_book_text(sys.argv[1])
     get_num_words(book_text)
     char_count = get_num_characters(book_text)
     sort_character_counts(char_count)
